beaa68c182b2a46488de0c3f42783dc7a4ba3639
Antonio Higor <higor.ant02@gmail.com>
2024-05-12 12:31:58 -0300

    Create files
    Create files

---
+++ SystemParking/ParkingMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SystemParking 
+{
+    public class ParkingMain
+    {
+        public static void Main(string[] args) 
+        {
+            double valueInitial = 0;
+            double valueHour = 0;
+            string place = "";
+            int option;
+
+            Console.WriteLine("Qual o valor inicial?");
+            valueInitial = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor por hora?");
+            valueHour = double.Parse(Console.ReadLine());
+
+            ParkingModel parking = new ParkingModel(valueInitial, valueHour);
+
+            do 
+            {
+                Console.WriteLine("Escolha uma opção do menu:");
+                Console.WriteLine("0- Cadastrar veículo;\n1- Listar Veículo;\n2- Remover veículo;\n3- Encerrar;");
+                option = int.Parse(Console.ReadLine());
+
+                switch(option) 
+                {
+                    case 0:
+                        Console.WriteLine("Qual a placa do veículo?");
+                        place = Console.ReadLine();
+                        parking.addCar(place);
+                        break;
+                    case 1:
+                        parking.printCar();
+                        break;
+                    case 2:
+                        Console.WriteLine("Qual veículo você deseja remover?");
+                        place = Console.ReadLine();
+                        parking.removeCar(place);
+                        break;
+                    default:
+                        Console.WriteLine("Não compreendi");
+                        break;
+                }
+            } while(option!=3);
+        }
+    }
+}
---
+++ SystemParking/models/ParkingModel.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+namespace SystemParking 
+{
+    public class ParkingModel 
+    {
+        private List<string> parking = new List<string>();
+        private double valueInitial;
+        private double valueHour;
+        public ParkingModel(double valueInitial, double valueHour)
+        {
+            this.valueInitial = valueInitial;
+            this.valueHour = valueHour;
+        }
+        public void setValueInitial(double valueInitial) 
+        {
+            this.valueInitial = valueInitial;
+        }
+        public double getValueInitial() 
+        {
+            return valueInitial;
+        }
+        public void setValueHour(double valueHour) 
+        {
+            this.valueHour = valueHour;
+        }
+        public double getValueHour() 
+        {
+            return valueHour;
+        }
+        public void addCar(string plate) 
+        {
+            parking.Add(plate);
+        }
+        public void removeCar(string place) {
+            int quantityHours;
+            foreach(string car in parking) 
+            {
+                if(car==place) 
+                {
+                    Console.WriteLine("Quantas horas o veículo passou no estacionamento?");
+                    quantityHours = int.Parse(Console.ReadLine());
+                    calculateValue(quantityHours);
+                    parking.Remove(place);
+                } 
+                else 
+                { 
+                    Console.WriteLine("Não existe registro dessa placa em nosso sistema");
+                }
+            }
+        }
+        public void calculateValue(int quantityHours) {
+            Console.Write("O veículo precisa pagar R$ " + getValueHour()*quantityHours + getValueInitial());
+        }
+        public void printCar() 
+        {
+            foreach(string car in parking) 
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
